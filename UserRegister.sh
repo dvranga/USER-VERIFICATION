530092e24022699aa83da0eb5bf882f20ecaca88
dvranga <devangmranganth>
2020-03-30 12:29:03 +0530

    [ranganath]Added: usercase3 required to validate the email

--- UserRegister.sh
+++ UserRegister.sh
@@ -5,9 +5,19 @@ function validate()
 pat="^[A-Z]{1}[a-zA-Z]{2}[a-zA-z0-9]*"
 if [[ $1 =~ $pat ]]
 then
-echo username is valid
+echo $1 is valid
 else
-echo username is not valid
+echo $1 is not valid
+fi
+}
+function validateEmail()
+{
+pat="^[a-zA-z]{3}[-_+.]{1}?[0-9]{3}?@(bridgelabz|gmail|yahoo|1|abc)(.com|.co|.net)(.au|.in)?$"
+if [[ $1 =~ $pat ]]
+then
+echo email is valid
+else
+echo "No! email is not valid"
 fi
 }
 echo enter the username to register
@@ -16,3 +26,6 @@ validate $username
 echo enter the lastname to register
 read lastname
 validate $lastname
+echo enter email to register
+read email
+validateEmail $email
