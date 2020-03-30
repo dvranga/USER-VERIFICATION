24359aa3d364661dae24bddd834351587df04eb8
dvranga <devangmranganth>
2020-03-30 16:53:10 +0530

    [ranganath]Added:password rule 2 atleast one uppercase

--- UserRegister.sh
+++ UserRegister.sh
@@ -40,7 +40,7 @@ echo "No! Mobile Number is not valid"
 fi
 echo enter password
 read password
-pat="^[a-zA-z0-9]{8,}$"
+pat="^[A-Z]{1}[a-zA-z0-9]{7,}$"
 if [[ $password  =~ $pat ]]
 then
 echo password is valid
