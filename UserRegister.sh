1bb4442f0d857bf9cc06f3c3b48feec67bd38ffa
dvranga <62434972+dvranga@users.noreply.github.com>
2020-03-30 15:47:41 +0530

    [ranganath]Added:mobile number with country code is checked"

--- UserRegister.sh
+++ UserRegister.sh
@@ -29,3 +29,12 @@ validate $lastname
 echo enter email to register
 read email
 validateEmail $email
+echo enter the mobile number to register
+read mobileNumber
+pat="^[91 ]{3}[6-9]{1}[0-9]{9}$"
+if [[ $mobileNumber =~ $pat ]]
+then
+echo "Mobile Number is valid"
+else
+echo "No! Mobile Number is not valid"
+fi
