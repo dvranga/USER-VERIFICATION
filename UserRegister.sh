665a9d6d1b689f6a8e79143f7692c589fd51fc8d
dvranga <62434972+dvranga@users.noreply.github.com>
2020-03-31 08:54:13 +0530

    [ranganath]Refacot::one digit and one uppercase need to accept

--- UserRegister.sh
+++ UserRegister.sh
@@ -40,7 +40,7 @@ echo "No! Mobile Number is not valid"
 fi
 function checkPassword()
 {
-pat="[a-zA-z0-9]{6,}[0-9A-Z]+|[0-9A-Z]+[a-zA-z0-9]{6,}"
+pat="^[a-zA-z0-9]{6,}[0-9A-Z]+|^[0-9A-Z]+[a-zA-z0-9]{6,}"
 if [[ $1  =~ $pat ]]
 then
 echo $1 is valid
